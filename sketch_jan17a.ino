f45aa0ce477dd442c859230d8a4e2376a041f7ea
brenton26-afk <77509555+brenton26-afk@users.noreply.github.com>
2023-01-18 12:18:11 -0500

    Add files via upload

---
+++ sketch_jan17a.ino
@@ -0,0 +1,35 @@
+
+
+void setup() {
+  // put your setup code here, to run once:
+    pinMode(12, OUTPUT);
+    pinMode(11, OUTPUT);
+    pinMode(10, OUTPUT);
+    pinMode(9, OUTPUT);
+    pinMode(8, OUTPUT);
+    pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+digitalWrite(8, HIGH);
+digitalWrite(12, HIGH);
+delay(1000);
+digitalWrite(8, LOW);
+digitalWrite(12, LOW);
+
+delay(3000);
+for(int i = 8; i <= 12; i += 2){    //i = i + 2
+  digitalWrite(i, HIGH);
+  delay(250);
+  digitalWrite(i, LOW);
+}
+for(int j = 12; j >= 8; j--){
+  digitalWrite(j, HIGH);
+  delay(250);
+  digitalWrite(j, LOW);
+}
+delay(1000);
+
+}
