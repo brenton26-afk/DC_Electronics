ea255ee2ea63fe24374ee91ad3845148429c03f0
brenton26-afk <77509555+brenton26-afk@users.noreply.github.com>
2023-01-27 12:39:39 -0500

    Add files via upload

---
+++ betterGame.ino
@@ -0,0 +1,81 @@
+int button = 0;
+int score = 0;
+int theTime = 3;
+
+long startTime;
+
+bool pressed = false;
+bool game = false;
+
+void setup(){
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(2, INPUT);
+  Serial.begin(9600);
+  Serial.println("Hello World");
+}
+
+void loop(){
+  button = digitalRead(2);
+  
+  if(pressed == false && button == HIGH){
+    score++;
+    pressed = true;
+    Serial.println(score);
+      
+      if(score == 1){
+        startTime = millis();
+        game = true;
+        Serial.println(startTime);
+        }
+  
+  } else if(button == LOW){
+    pressed = false;
+  }
+
+  digitalWrite(7, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(4, HIGH);
+
+    
+   if(millis() - startTime >= 3000 && game == true){
+     digitalWrite(7, LOW);
+   }
+
+   if(millis() - startTime >= 4000 && game == true){
+    digitalWrite(6, LOW);
+   }
+
+   if(millis() - startTime >= 5000 && game == true){
+    digitalWrite(4,LOW);
+   }
+
+    if(score >= 5){
+  digitalWrite(8, HIGH);
+  }
+  
+if(score >= 10){
+  digitalWrite(9, HIGH);
+  }
+  
+if(score >= 15){
+  digitalWrite(10, HIGH);
+  }
+  
+if(score >= 20){
+  digitalWrite(11, HIGH);
+  }
+  
+if(score >= 25){
+  digitalWrite(12, HIGH);
+  }
+
+
+
+}
